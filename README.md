2b8560c05c895137c79c1cb0ac8411fe673f81ce
Leehun <lehuannhatrang98@gmail.com>
2024-12-22 20:58:19 +0900

    update docker file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# lipo-solar-prediction-system
+# Lipo Solar Prediction System
+
+Start to deploy:
+```
+docker-compose build
+docker-compose up -d  
+```
--- frontend/pages/Collections.py
+++ frontend/pages/Collections.py
@@ -10,7 +10,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-st.sidebar.header('Forecast')
+st.sidebar.header('Collections')
 
 if 'query_data' not in st.session_state:
     st.session_state.query_data = None
