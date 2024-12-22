400a6a1714f0676aa0b757b61e7a35068564df70
Leehun <lehuannhatrang98@gmail.com>
2024-12-22 19:49:50 +0900

    fix title of page

--- frontend/pages/Forecast.py
+++ frontend/pages/Forecast.py
@@ -11,7 +11,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-st.sidebar.header('Collections')
+st.sidebar.header('Forecast')
 
 if 'predict_data' not in st.session_state:
     st.session_state.predict_data = None
