92411f232b7227fc86de6ad421057cad60f1801d
Leehun <lehuannhatrang98@gmail.com>
2025-01-23 14:08:22 +0700

    update inference code

--- ai_core/forecast/inference_code/NER/predictor.py
+++ ai_core/forecast/inference_code/NER/predictor.py
@@ -8,22 +8,15 @@ import numpy as np
 # Initialize the Flask app
 app = Flask(__name__)
 
-model = joblib.load(os.path.join(os.environ["SM_MODEL_DIR"], "model.joblib"))
+# model = joblib.load(os.path.join(os.environ["SM_MODEL_DIR"], "model.joblib"))
 
-# model_path = os.path.join('/opt/ml/model', 'model.pkl')
+model_path = os.path.join('/opt/ml/model', 'model.pkl')
 # model_path = "model/model.pkl"
 
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
-
-# Load the pre-trained model from file
-# with open(model_path, 'rb') as model_file:
-#     model = pickle.load(model_file)
 
 # Define the route for inference requests
 @app.route('/invocations', methods=['POST'])
-def predict():
+def predictor():
     try:
         # Get input JSON from the request
         data = request.get_json()
@@ -41,10 +34,7 @@ def predict():
 @app.route('/ping', methods=['GET'])
 def ping():
     try:
-        return "Healthy", 200
+        return Response(response= '\n', status=200, mimetype='application/json')
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
